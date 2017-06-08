b4ff5a9728763ce78f4cfcc695e403cdef8b834b
JoseLeonTM <jleon@tekmexico.com>
2017-06-08 11:48:21 -0700

    Finished exercise 226

---
+++ 226/226.ts
@@ -0,0 +1,21 @@
+
+
+var a = $('a');
+
+
+a.on('click',request);
+
+
+function request(ev){
+    ev.preventDefault();
+    var link = $(ev.target)[0].attributes[0].nodeValue;
+    // var link = $(ev.target);
+    $.ajax({
+        url:link
+    }).then(function(res){
+        console.log(res);
+    },function(er){
+        console.log('Error: ',er);
+        window.location.replace('error.html');
+    });
+}
